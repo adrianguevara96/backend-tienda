86129c718b4c7fee2c7683c80cc442de3f3cf8eb
Adrian Guevara <adriangja96@gmail.com>
2022-11-01 00:09:02 -0400

    Class 2 - Routing

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
   res.send('Hello, my server in express');
 })
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hello, Im a new endpoint');
+})
+
+app.get('/products', (req, res) => {
+  res.json({
+    id: 1,
+    name: 'Product 1',
+    price: 1000
+  })
+})
+
 app.listen(port, () => {
   console.log("Port: " + port);
 })
