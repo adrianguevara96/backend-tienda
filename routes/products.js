84bd01a19110486876d9e63efd284d2031876c16
Adrian Guevara <adrianjga96@gmail.com>
2022-12-14 13:50:38 +0100

    Clase 15 - Async, await y captura de datos

--- routes/products.js
+++ routes/products.js
@@ -5,8 +5,8 @@ const ProductsService = require('./services/product.service')
 const service = new ProductsService()
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  const products = service.find();
+router.get('/', async (req, res) => {
+  const products = await service.find();
   res.json(products);
 })
 
@@ -14,39 +14,46 @@ router.get('/filter', (req, res) => {
   res.send('Yo soy un filter');
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  const product = service.findOne(id);
+  const product = await service.findOne(id);
   res.json(product);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   //receive data
   const body = req.body;
-  const newProduct = service.create(body);
+  const newProduct = await service.create(body);
 
   res.status(201).json({
     newProduct
   })
 });
 
-router.patch('/:id', (req, res) => {
-  //params
-  const { id } = req.params;
-  //receive data
-  const body = req.body;
-  const product = service.update(id, body);
-  res.json({
-    message: 'update',
-    data: product,
-    id,
-  })
+router.patch('/:id', async (req, res) => {
+  try {
+    //params
+    const { id } = req.params;
+    //receive data
+    const body = req.body;
+    const product = await service.update(id, body);
+    res.json({
+      message: 'update',
+      data: product,
+      id,
+    })
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    });
+  }
+
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   //params
   const { id } = req.params;
-  const product = service.delete(id);
+  const product = await service.delete(id);
   //response
   res.json({
     message: 'deleted',
