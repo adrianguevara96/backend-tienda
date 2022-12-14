60a373d969a468b33a918c556e52a9f438401777
Adrian Guevara <adrianjga96@gmail.com>
2022-12-14 16:42:06 +0100

    Clase 17 - Middleware for Errors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const routerApi = require('./routes');
 
+//Middleware
+const { logError, errorHandler } = require('./middlewares/error.handler');
+
 const app = express();
 const port = 3000;
 
@@ -9,6 +12,10 @@ app.use(express.json());
 
 //Routers
 routerApi(app);
+
+//Middleware
+app.use(logError);
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
   res.send('Hello, my server in express');
