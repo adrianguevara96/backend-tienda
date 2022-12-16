5c6e9b60beb52fc8d36474442d8a13da10aacf68
Adrian Guevara <adrianjga96@gmail.com>
2022-12-16 13:30:30 +0100

    process env port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { logError, errorHandler, boomErrorHandler } = require('./middlewares/erro
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //middleware JSON
 app.use(express.json());
